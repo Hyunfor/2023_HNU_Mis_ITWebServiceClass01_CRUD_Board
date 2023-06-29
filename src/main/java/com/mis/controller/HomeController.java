ac0c554e0d66eebdc0055cbda3fac6152d2fd61f
Hyunfor <navel5148@gmail.com>
2023-06-29 10:22:04 +0900

    업로드 테스트 및 BOARD DB 추가

--- src/main/java/com/mis/controller/HomeController.java
+++ src/main/java/com/mis/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.mis.web;
+package com.mis.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
--- src/main/java/com/mis/controller/SampleController.java
+++ src/main/java/com/mis/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.mis.web;
+package com.mis.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
