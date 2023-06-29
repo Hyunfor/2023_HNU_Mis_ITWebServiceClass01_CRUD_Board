01fe9a6499c569df914286391950866e2418f1ff
Hyunfor <navel5148@gmail.com>
2023-06-29 13:13:59 +0900

    BoardController 생성 및 작업, board, success.jsp 작업

---
+++ src/main/java/com/mis/controller/BoardController.java
@@ -0,0 +1,45 @@
+package com.mis.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mis.domain.BoardVO;
+import com.mis.service.BoardService;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+	@Inject
+	private BoardService service;
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET() throws Exception {
+
+		logger.info("register get ...");
+
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardVO vo, Model model) throws Exception {
+		
+		logger.info("register post ...");
+		
+		// 게시글 등록
+		service.register(vo);
+		
+		// 게시글 성공 화면으로 이동 + 메시지 추가
+		model.addAttribute("result", "success");
+
+		return "/board/success";
+
+	}
+}
