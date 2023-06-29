f9f764568cbbf4347b6cc1f4bf80191326a8dc5b
Hyunfor <navel5148@gmail.com>
2023-06-29 12:39:22 +0900

    BoardServiceImpl 생성 및 작업

--- src/main/java/com/mis/service/BoardService.java
+++ src/main/java/com/mis/service/BoardService.java
@@ -5,11 +5,15 @@ import java.util.List;
 import com.mis.domain.BoardVO;
 
 public interface BoardService {
-	
+
 	public void register(BoardVO vo) throws Exception;
+
 	public BoardVO read(int bno) throws Exception;
+
 	public void moidfy(BoardVO vo) throws Exception;
+
 	public void remove(int bno) throws Exception;
+
 	public List<BoardVO> listAll() throws Exception;
 
 }
